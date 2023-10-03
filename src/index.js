a5889bf96f1f3ca7bbbc47536224fcec3f1d61a5
Nelly Chepkemoi <nellychepkem2015@gmail.com>
2023-10-03 08:15:05 +0300

    completed assignments

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  //const button = document.getElementById("new-task-description")
+  //console.log(id)
+  let form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let todo = document.getElementById("new-task-description").value;
+    //console.log(todo)
+    addTodo(todo);
+  });
+
+  function addTodo(todo) {
+    let newItem = document.createElement("li");
+    newItem.textContent = todo;
+    //console.log(newItem)
+    document.getElementById("tasks").appendChild(newItem);
+  }
 });
